f91b0844d7e8b6e21c8fab2cdf5c6428a47e3f5c
sls1005 <710052@mail.pcsh.ntpc.edu.tw>
2022-04-22 21:04:29 +0800

    implemented TypeList in another way

--- src/cppclass/typeLists.nim
+++ src/cppclass/typeLists.nim
@@ -1,31 +1,23 @@
 import std/macros
 
-type TypeList* = object
-  types, names: seq[NimNode]
-
-proc len*(self: TypeList): int {.compileTime.} =
-  result = len(self.types)
-  assert result == len(self.names)
+type TypeList* = seq[array[2, NimNode]]
 
 proc identify*(self: var TypeList, typ: NimNode): NimNode {.compileTime.} =
   if typ.kind in [nnkIdent, nnkSym]:
     return typ
-  for i, t in self.types:
-    if t == typ:
-      let name = self.names[i]
-      return name
+  for record in self:
+    if record[1] == typ:
+      return record[0]
   let name = genSym(nskType)
-  self.types.add(typ)
-  self.names.add(name)
+  self.add([name, typ])
   return name
 
-proc toNimNode*(self: sink TypeList): NimNode {.compileTime.} =
+proc toNimNode*(self: TypeList): NimNode {.compileTime.} =
   result = newNimNode(nnkTypeSection)
-  for i in 0 ..< len(self):
+  for record in self:
     result.add newTree(
       nnkTypeDef,
-      self.names[i],
+      record[0], #name
       newEmptyNode(),
-      self.types[i]
+      record[1] #type
     )
-
